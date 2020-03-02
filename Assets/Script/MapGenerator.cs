0a1581eab266183cadfcdb6ea2c6a97e7ebcf343
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-03-03 10:08:47 +1100

    adding pseudocode

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    //set an array of prefab terrain (to increase frequency add a prefab multiple times)
+    //set a grid of empty hexagonal spaces to fill in with the prefabs
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //for each empty spaces instantiate terrain from array.
+        //use a randomizer in the range of terrain.length to decide which terrain to spawn at each point
+            
     }
 
     // Update is called once per frame
--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    //designate a max movement and cur movement available
+    //create costs moving into a map tile based on which terrain type
+    //extend that to other tiles
+    //set active area highlights you can go based on cur movement remaining and on enemies based
+    //write a pathfinding algorithm
+    //set easy terrain to 1 cost,difficult to 2 cost and impassable terrain to 9001 cost
+        //make Flyers set all terrain cost to 1
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    // reset movement to max at start of turn
     // Update is called once per frame
     void Update()
     {
