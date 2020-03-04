87dcd00dce4b576702e133b1ecc3979ff8b22c13
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-03-04 11:01:27 +1100

    added tile manager pseudocode

---
+++ Assets/Script/TileManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    // add terrain cost int
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
