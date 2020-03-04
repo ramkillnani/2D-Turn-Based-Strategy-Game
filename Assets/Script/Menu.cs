3e0c2c496cc257118b7efbed70e1612578cfa0a2
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-03-04 13:50:34 +1100

    commited

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
@@ -18,8 +19,62 @@ public class Menu : MonoBehaviour
     }
     public void TogglePause()
     {
-
+        if (isPaused == true)
+        {
+           
+            Pause_menu.SetActive(false);
+            isPaused = false;
+            return;
+        }
+        else
+        {
+            
+            Pause_menu.SetActive(true);
+            isPaused = true;
+            return;
+        }
     }
+    public void ToggleSetting(bool on)
+    {
+        if (on == true)
+        {
+            Settings_menu.SetActive(false);
+           
+            return;
+
+        }
+        else
+        {
+            Settings_menu.SetActive(true);
+            
+            return;
+        }
+        
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+    }
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+    }
+    public void Prevlevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1, LoadSceneMode.Single);
+    }
+    public void ChangeLevel(int index)
+    {
+        SceneManager.LoadScene(index, LoadSceneMode.Single);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
     //create a function that toggles pause on and off
     //make timescale 0 when paused and 1 when unpaused
     //set pause menu active when paused and inactive when unpaused
@@ -33,6 +88,10 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+
+        }
     }
 }
