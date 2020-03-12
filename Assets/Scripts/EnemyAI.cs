e05ae8bf9f71cd76ce0bee8dd4d7b61c63cc9e65
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-03-13 10:22:38 +1100

    g

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class EnemyAI : MonoBehaviour
+{
+    //desiginates who the player is
+    public Transform target;
+    public float moveSpeed;
+    public NavMeshAgent agent;
+    
+
+
+    public GameObject self;
+
+    public float turnSpeed;
+
+
+
+
+
+    public Rigidbody rigid;
+
+
+    void Start()
+    {
+
+
+        agent = self.GetComponent<NavMeshAgent>();
+
+
+
+    }
+
+    void Update()
+    {
+
+
+
+
+
+
+
+
+       
+
+
+
+            transform.position = Vector3.MoveTowards(transform.position, target.position, 0.2f);
+            agent.destination = target.position;
+
+
+
+
+
+        
+
+    }
+}
+
+
