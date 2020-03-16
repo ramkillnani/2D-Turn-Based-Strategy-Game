ef12286401980b29527ee3aba43bbf441100c700
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2020-03-16 14:29:08 +1100

    woorked on getting XML saving

---
+++ Assets/Scripts/Saving/Saving.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class Saving : MonoBehaviour
+{
+    public Scores[] high = new Scores[10]; //New Scores high
+    public void Awake()
+    {
+        //Load Data saved
+        LoadData();
+    }
+
+    public void LoadData()
+    {
+        //Load the data from the XML File
+        SaveData data = XMLSaving.ReadData();
+        //If data isnt equal to null
+        if (data != null)
+        {
+            //for all high
+            for (int i = 0; i < high.Length; i++)
+            {
+                //Set the player in high to player playername in data 
+                high[i].player = data.playerName[i];
+                //set the wave in high to the wave in data
+                high[i].dis = data.level[i];
+                high[i].dis = data.turn[i];
+            }
+        }
+        else //else if data is null
+        {
+            //for each high
+            for (int i = 0; i < high.Length; i++)
+            {
+                //Set the player to blank
+                high[i].player = "Blank";
+                //Set the wave to zero
+                high[i].dis = 0;
+            }
+        }
+        //Sort the score data
+        Sort();
+    }
+
+    //When a new score is added
+    public void NewScore(string name, int number)
+    {
+        //Set the wave 0 to the number
+        high[0].dis = number;
+        //set the player to equal name
+        high[0].player = name;
+        //Sort
+        Sort();
+    }
+
+    public void Sort()
+    {
+        //Sort the strut in a desending order
+        Array.Sort(high, (x, y) => x.dis.CompareTo(y.dis));
+        //Save the data
+        Save();
+    }
+
+    //Save the highscore data
+    public void Save()
+    {
+        //New Highscores class under the refernce data
+        SaveData data = new SaveData();
+        //New temp string name
+        string[] name = new string[10];
+        //New temp string number
+        int[] number = new int[10];
+        //For every strut in the high array
+        for (int i = 0; i < high.Length; i++)
+        {
+            //Set data in playername in location i of the array to high in location i of the struts string player
+            data.playerName[i] = high[i].player;
+            //Set data in wave in location i of the array to high in location i of the struts int wave
+            data.level[i] = high[i].dis;
+            data.turn[i] = high[i].dis;
+        }
+        //Write the Variable data to an XML file
+        XMLSaving.WriteData(data);
+    }
+}
+
+[System.Serializable]
+public struct Scores
+{
+    public string player; //String for player names
+    public int dis; //int for ammount of waves done
+}
---
+++ Assets/Scripts/Saving/XMLSaving.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+using UnityEngine;
+
+public static class XMLSaving
+{
+    public static SaveData ReadData()
+    {
+        //New string path that is the applications data path with /Highscore.sav
+        string path = Application.persistentDataPath + "/SaveData.XML";
+        //If the file path exists
+        if (File.Exists(path))
+        {
+            //New XML serializer for highsocores data
+            var serializer = new XmlSerializer(typeof(SaveData));
+            //New file stream to open file at path location
+            var stream = new FileStream(path, FileMode.Open);
+            //New container for the data to deserialize as highscores
+            var container = serializer.Deserialize(stream) as SaveData;
+            //Close the stream
+            stream.Close();
+            //Return the container
+            return container;
+        }
+        else
+        {
+            //Return null
+            return null;
+        }
+    }
+
+    public static void WriteData(SaveData data)
+    {
+        //New XML serializer for highsocores data
+        var serializer = new XmlSerializer(typeof(SaveData));
+        //New string path that is the applications data path with /Highscore.sav
+        string path = Application.persistentDataPath + "/SaveData.XML";
+        //New file stream at path location
+        var stream = new FileStream(path, FileMode.Create);
+        //serialze the data
+        serializer.Serialize(stream, data);
+        //Close Stream
+        stream.Close();
+    }
+}
